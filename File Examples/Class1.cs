6efc23ddd3be3d38701299a0253fcc2db96a14c7
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-11-17 15:43:53 +0530

    [Shreesha] Added File input output

---
+++ File Examples/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace File_Examples
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            FileInfo f = new FileInfo(@"D:\File In-out\demo.txt");
+            using (StreamWriter sw = f.CreateText())
+            {
+                sw.WriteLine("This is a demo file");
+                sw.WriteLine("Second line of demo file");
+            }
+            using (StreamReader sr = f.OpenText())
+            {
+                string data=sr.ReadLine();
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = sr.ReadLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ File Examples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace File_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (File.Create(@"D:\File In-out\sample.txt")) {
+                Console.WriteLine("File created successfully");
+            }
+            using (StreamWriter sw = new StreamWriter(@"D:\File In-out\sample.txt")) { 
+            sw.WriteLine("This is a sample file");
+            sw.WriteLine("Second line of the file");
+        }
+            using (StreamReader sr = new StreamReader(@"D:\File In-out\sample.txt"))
+            {
+
+
+                string data = sr.ReadLine();
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = sr.ReadLine();
+                }
+            }
+            File.Copy(@"D:\File In-out\sample.txt", @"D:\File In-out\temp.txt",true);
+           
+
+
+        }
+    }
+}
